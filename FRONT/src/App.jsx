9a2a0dde52747d8824e2210527f347d981a98ff9
Esteban Samaniego <107878186+MaxiSama12@users.noreply.github.com>
2025-06-10 19:44:06 -0300

    Vaciamos los componentes basicos del proyecto para empezar a trabajar cada uno en la rama develop

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,7 +1,5 @@
-import {BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
-import HomePages from './pages/HomePages'
-import LoginPages from './pages/LoginPages'
-import { HOME, LOGIN } from './router/route'
+import {Routes, Route} from 'react-router-dom'
+
 
 
 function App() {
@@ -11,8 +9,9 @@ function App() {
     
     <BrowserRouter>
     <Routes>
-      <Route path={HOME} element={<HomePages/>} />
-      <Route path={LOGIN} element={<LoginPages/>} />
+
+
+
     </Routes>
     </BrowserRouter>
 
