139ceaf4c70f8a191eb27a4342c1c3cea8ddfd6c
Esteban Samaniego <107878186+MaxiSama12@users.noreply.github.com>
2025-06-11 22:49:50 -0300

    Se agregaron datos en la db.json, logica al login y endpoints

--- FRONT/src/components/auth/LoginForm.jsx
+++ FRONT/src/components/auth/LoginForm.jsx
@@ -1,27 +1,64 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../../styles/LoginForm.css";
+import { useNavigate } from "react-router-dom";
+import {
+  DASHBOARDVENDEDOR,
+  HOME,
+  REGISTERCLIENTE,
+  REGISTERREPARTIDOR,
+  REGISTERVENDEDOR,
+} from "../../router/route";
+import { CLIENTES } from "../../endpoints/endpoints";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+  const [loginError, setLoginError] = useState("");
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    role: "",
   });
-
-  
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    alert("Inicio de sesión exitoso");
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.get(CLIENTES);
+      const users = res.data;
+
+      //en este codigo se verifica el mail y la contraseña ingresadas y devuelve un error personalizado si no se encuentra el mail o la contraseña
+      const userFound = users.find((user) => user.email === formData.email);
+      if (userFound) {
+        if (userFound.password === formData.password) {
+          console.log("el usuario encontrado fue: ", userFound);
+          alert(
+            `Inicio de sesión exitoso, bienvenido ${userFound.name}`
+          );
+
+          if (userFound.rol === "cliente") {
+            navigate(HOME);
+          } else if (userFound.rol === "vendedor") {
+            navigate(DASHBOARDVENDEDOR);
+          }
+        } else {
+          setLoginError("la contraseña es incorrecta");
+        }
+      } else {
+        setLoginError("la contraseña es incorrecta");
+      }
+    } catch (error) {
+      console.log("error trayendo los usuarios:", error);
+    }
   };
 
   return (
     <form className="login-form" onSubmit={handleSubmit}>
-      <h2 className="login-title">Iniciar Sesión</h2>
+      <h2 className="login-title">
+        <b>Iniciar Sesión</b>
+      </h2>
 
       <label>
         Correo electrónico:
@@ -31,7 +68,9 @@ const LoginForm = () => {
           value={formData.email}
           onChange={handleChange}
           className="login-input"
-        /> 
+          required
+        />
+        {formData.email}
       </label>
 
       <label>
@@ -42,19 +81,22 @@ const LoginForm = () => {
           value={formData.password}
           onChange={handleChange}
           className="login-input"
-        /> 
+          required
+        />
+        {formData.password}
       </label>
-
-
       <button type="submit" className="login-button">
         Ingresar
       </button>
+      <p className="text-center">
+        <b>{loginError}</b>
+      </p>
 
       <div className="login-links">
         <p>¿No tienes una cuenta?</p>
-        <a href="/registro-cliente">Registrarse como Cliente</a>
-        <a href="/registro-vendedor">Registrarse como Vendedor</a>
-        <a href="/registro-repartidor">Registrarse como Repartidor</a>
+        <a href={REGISTERCLIENTE}>Registrate como Cliente</a>
+        <a href={REGISTERVENDEDOR}>Registrate como Vendedor</a>
+        <a href={REGISTERREPARTIDOR}>Registrate como Repartidor</a>
       </div>
     </form>
   );
---
+++ FRONT/src/endpoints/endpoints.js
@@ -0,0 +1,4 @@
+// Utilizados en json-server para pruebas.
+export const CLIENTES = "http://localhost:3000/clientes";
+export const VENDEDORES = "http://localhost:3000/vendedores";
+export const REPARTIDORES = "http://localhost:3000/repartidores";
