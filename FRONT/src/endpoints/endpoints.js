4e14b4a31d796530a3b22d261af8005ef1f1076d
Esteban Samaniego <107878186+MaxiSama12@users.noreply.github.com>
2025-06-14 17:15:10 -0300

    Agregamos endpoints para las consultas de rubros, comercios y vendedores

--- FRONT/src/endpoints/endpoints.js
+++ FRONT/src/endpoints/endpoints.js
@@ -1,4 +1,6 @@
 // Utilizados en json-server para pruebas.
 export const CLIENTES = "http://localhost:3000/clientes";
-export const VENDEDORES = "http://localhost:3000/vendedores";
 export const REPARTIDORES = "http://localhost:3000/repartidores";
+export const VENDEDORES = "http://localhost:3000/vendedores";
+export const COMERCIOS = "http://localhost:3000/comercios";
+export const RUBROS = "http://localhost:3000/rubros";
