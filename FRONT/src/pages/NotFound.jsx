ed9ce4a1c26cf2f9671b3382dc4a9c6542c805af
Juan Daniel Morales <juandanielmoralesxd@gmail.com>
2025-06-15 00:23:15 -0300

    Estilo de NotFound y últimos detalles

--- FRONT/src/pages/NotFound.jsx
+++ FRONT/src/pages/NotFound.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
+import '../styles/NotFound.css';
+import notfound from '../assets/notFound/notfound.png';
 
 const NotFound = () => {
     return (
         <div>
             <Navbar />
-            <h1>Error 404!</h1>
-            <p>Page Not Found</p>
-            <p>Lo sentimos, la página que estás buscando no existe.</p>
-            <p>Por favor, verifica la URL o vuelve a la página de <Link to="/">Inicio</Link>.</p>   
-            <p>Si necesitas ayuda, contacta con el soporte técnico.</p>
-            <p>Gracias por tu comprensión.</p>
+            <main className="not-found-content">
+                <div className="content-wrapper">
+                    <img src={notfound} alt="Error 404" className="not-found-image" />
+                    <div className="text-content">
+                        <h1>Error 404!</h1>
+                        <p>Page Not Found</p>
+                        <p>Lo sentimos, la página que estás buscando no existe.</p>
+                        <p>Por favor, verifica la URL o vuelve a la página de <Link to="/">Inicio</Link>.</p>
+                        <p>Si necesitas ayuda, contacta con el soporte técnico.</p>
+                        <p>Gracias por tu comprensión.</p>
+                    </div>
+                </div>
+            </main>
             <Footer />
         </div>
     );
