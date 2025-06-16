d2c1fd6ee7174e3fd1a29c551cf7b25d3d36d803
Franco Giacobbe <frandg47@gmail.com>
2025-06-15 22:24:10 -0300

    actualizando home con datos de db.json

--- FRONT/src/components/ui/CatalogoHomePage.jsx
+++ FRONT/src/components/ui/CatalogoHomePage.jsx
@@ -1,120 +1,30 @@
 import "../../styles/home.css";
 import Card from "./Card";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { PRODUCTOS } from "../../endpoints/endpoints";
+import Swal from "sweetalert2";
 
 const CatalogoHomePage = () => {
-  const productos = [
-    {
-      id: 1,
-      title: "Burger Indiapolis",
-      description: "Delicious burger with a unique twist.",
-      price: 12.99,
-      stars: 4.5,
-      urlImage:
-        "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/24P2OKC3RVEHRD3F2VKQ76XX7M.jpg",
-      category: "comida",
-    },
-    {
-      id: 2,
-      title: "Pizza Margherita",
-      description: "Classic pizza with fresh ingredients.",
-      price: 10.99,
-      stars: 4.0,
-      urlImage:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTY7RbPLpeQGEKr-JMLB6L9kRdCTMtUbFJfJw&s",
-      category: "comida",
-    },
-    {
-      id: 3,
-      title: "Sushi Platter",
-      description: "Assorted sushi rolls with fresh fish.",
-      price: 15.99,
-      stars: 4.8,
-      urlImage:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2jvMS-TgX4acPMeLqoZCnJht27NJhkKQW3A&s",
-      category: "comida",
-    },
-    {
-      id: 4,
-      title: "Fideos x 500g",
-      description: "Pasta seca de trigo candeal.",
-      price: 2.5,
-      stars: 4.3,
-      urlImage:
-        "https://acdn-us.mitiendanube.com/stores/798/865/products/106815563-2a0f2d53946e5e557f16994272770401-640-0.jpg",
-      category: "supermercado",
-    },
-    {
-      id: 5,
-      title: "Galletas Oreo",
-      description: "Galletas Dulces x 354g",
-      price: 3.2,
-      stars: 4.6,
-      urlImage:
-        "https://masonlineprod.vtexassets.com/arquivos/ids/316379/Galletitas-Oreo-Regular-Tripack-354g-2-42387.jpg?v=638757387211700000",
-      category: "supermercado",
-    },
-    {
-      id: 6,
-      title: "Agua Villavicencio 1.5L",
-      description: "Agua Mineral Sin Gas",
-      price: 1.8,
-      stars: 4.2,
-      urlImage:
-        "https://acdn-us.mitiendanube.com/stores/001/157/846/products/556225-800-auto11-6873ce0acf40df215b16354448519602-1024-1024.jpg",
-      category: "supermercado",
-    },
-    {
-      id: 7,
-      title: "Jugo Citric x 1L",
-      description: "Jugo exprimido 100% Natural",
-      price: 2.9,
-      stars: 4.5,
-      urlImage:
-        "https://http2.mlstatic.com/D_NQ_NP_679509-MLA46881414167_072021-O.webp",
-      category: "supermercado",
-    },
-    {
-      id: 8,
-      title: "Tafirol 500mg",
-      description: "Analgésico y antipirético x 30 comprimidos",
-      price: 1.5,
-      stars: 4.8,
-      urlImage: "https://tafirol.com/hubfs/Tafirol-500.png",
-      category: "medicamentos",
-    },
-    {
-      id: 9,
-      title: "Alcohol en Gel 250ml",
-      description: "Desinfectante instantáneo para manos con 70% de alcohol.",
-      price: 2.3,
-      stars: 4.7,
-      urlImage:
-        "https://acdn-us.mitiendanube.com/stores/001/130/470/products/algabo-ultra-x250-ml-alcohol-en-gel-77912742002431-3fe19bb1061aa7eda916487618099237-640-0.png",
-      category: "medicamentos",
-    },
-    {
-      id: 10,
-      title: "Cuaderno Rivadavia",
-      description: "Tapa dura, A4 x 80 hojas rayadas",
-      price: 4.0,
-      stars: 4.4,
-      urlImage:
-        "https://acdn-us.mitiendanube.com/stores/001/398/366/products/abc-a4-rayado-60-a518098ca15a1ee57917024203804500-640-0.jpg",
-      category: "librería",
-    },
-    {
-      id: 11,
-      title: "Bolígrafos Bic (pack x3)",
-      description: "Tinta fluida, cuerpo ergonómico. Escritura suave.",
-      price: 1.2,
-      stars: 4.3,
-      urlImage:
-        "https://tiotomar.vtexassets.com/arquivos/ids/174508-800-800?v=637937591748630000&width=800&height=800&aspect=true",
-      category: "librería",
-    },
-  ];
+  
   const [activeFilter, setActiveFilter] = useState("todos");
+  const [productos, setProductos] = useState([]);
+  const getProductos = async () => {
+    try {
+      const res = await axios.get(`${PRODUCTOS}`);
+      setProductos(res.data);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error al cargar los productos",
+        text: error.response?.data?.mensaje || "Error desconocido",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getProductos();
+  }, []);
 
   const filteredProducts =
     activeFilter === "todos"
@@ -145,7 +55,7 @@ const CatalogoHomePage = () => {
         </div>
         <div className="card-container">
           {filteredProducts.map((producto) => (
-            <Card key={producto.id} {...producto} />
+            <Card producto={producto} />
           ))}
         </div>
       </div>
--- FRONT/src/components/ui/CommerceCarousel.jsx
+++ FRONT/src/components/ui/CommerceCarousel.jsx
@@ -5,45 +5,31 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { FaStar } from "react-icons/fa";
 import "../../styles/carousel.css";
+import { useState, useEffect } from "react";
+import { COMERCIOS } from "../../endpoints/endpoints";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const TopRatedStoresCarousel = () => {
-  const stores = [
-    {
-      name: "Supermercado Vea",
-      category: "Supermercado",
-      image:
-        "https://www.webretail.com.ar/wp-content/uploads/2023/04/Vea-Cencosud.jpg",
-      rating: 4.7,
-    },
-    {
-      name: "Farmacia del Pueblo",
-      category: "Farmacia",
-      image:
-        "https://images.rappi.com.ar/marketplace/store_type_1626975357300.jpg",
-      rating: 4.9,
-    },
-    {
-      name: "Librería San Pablo",
-      category: "Librería",
-      image:
-        "https://recursos-we-us-east-1.s3.amazonaws.com/Weyop/LSP/Contenidos/2024-08-15%2015%3A25%3A51/descarga%20%284%29.png",
-      rating: 4.6,
-    },
-    {
-      name: "Mc Donald's",
-      category: "Comida Rápida",
-      image:
-        "https://cdn.aarp.net/content/dam/aarpe/es/home/trabajo/pequenos-negocios/info-2021/comprar-franquicia-de-McDonalds/_jcr_content/root/container_main/container_body_main/container_body1/container_body_cf/container_image/articlecontentfragment/cfimage.coreimg.50.932.jpeg/content/dam/aarp/work/work_at_plus/2021/09/1140-mcdonalds-esp.jpg",
-      rating: 4.8,
-    },
-    {
-      name: "La Pizzada",
-      category: "Pizzería",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0D7HfefHBN8fEVhms12IV_7Wfx7mBbwqANg&s",
-      rating: 4.5,
-    },
-  ];
+  const [comercios, setComercios] = useState([]);
+
+  const getComercios = async () => {
+    try {
+      const res = await axios.get(`${COMERCIOS}`);
+      setComercios(res.data);
+      console.log("Comercios cargados:", res.data);
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Error al cargar los comercios",
+        text: error.response?.data?.mensaje || "Error desconocido",
+      });
+    }
+  };
+
+  useEffect(() => {
+    getComercios();
+  }, []);
 
   return (
     <section id="top-rated-stores" className="position-relative py-4">
@@ -65,16 +51,16 @@ const TopRatedStoresCarousel = () => {
             1280: { slidesPerView: 4, spaceBetween: 50 },
           }}
         >
-          {stores.map((store, index) => (
+          {comercios.map((comercio, index) => (
             <SwiperSlide key={index}>
               <div className="store-card mt-5">
                 <img
-                  src={store.image}
-                  alt={store.name}
+                  src={comercio.url_imagen}
+                  alt={comercio.nombre}
                   className="store-image"
                 />
                 <div className="rating-badge">
-                  <FaStar className="me-1  star" /> {store.rating.toFixed(1)}
+                  <FaStar className="me-1  star" /> {comercio.rating.toFixed(1)}
                 </div>
               </div>
             </SwiperSlide>
