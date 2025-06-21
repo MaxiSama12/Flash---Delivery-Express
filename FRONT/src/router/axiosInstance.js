96d6a96a399749b49dd37611340a5d793aa2a75b
Esteban Samaniego <107878186+MaxiSama12@users.noreply.github.com>
2025-06-21 19:20:45 -0300

    Se agregó una funcion para conectarse remotamente a otro localhost

--- FRONT/src/router/axiosInstance.js
+++ FRONT/src/router/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const puerto = 8080;
 
 export const axiosInstance = axios.create({
-    baseURL: "https://31dd-2803-9800-9444-76e6-8592-d5e8-557d-ad15.ngrok-free.app",
+    baseURL: "https://91f3-2803-9800-9444-76e6-8592-d5e8-557d-ad15.ngrok-free.app/",
     headers: {
     "ngrok-skip-browser-warning": "true", 
   }
