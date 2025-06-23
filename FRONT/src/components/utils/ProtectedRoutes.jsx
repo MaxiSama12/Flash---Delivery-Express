a975090d7d7387d5f2b7bd60fc840acfc5d3155b
Franco Giacobbe <frandg47@gmail.com>
2025-06-23 00:27:18 -0300

    arreglando problemas de carrito

--- FRONT/src/components/utils/ProtectedRoutes.jsx
+++ FRONT/src/components/utils/ProtectedRoutes.jsx
@@ -1,5 +1,5 @@
 import { useAuthStore } from "../../store/authStore";
-import { Navigate, useLocation, useParams } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function ProtectedRoutes({ children, allowedRoles }) {
   const user = useAuthStore((state) => state.usuario);
