d5c35b05a2e823415097e674afc964f5cecc1347
Franco Giacobbe <frandg47@gmail.com>
2025-07-07 13:42:55 -0300

    linkeando back deployado

--- FRONT/src/router/axiosInstance.js
+++ FRONT/src/router/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // const puerto = 8080;
 
 export const axiosInstance = axios.create({
-    baseURL: `http://localhost:31195`,
+    baseURL: `https://flash-delivery-express-back.onrender.com`,
     headers: {
     "ngrok-skip-browser-warning": "true", 
   }
