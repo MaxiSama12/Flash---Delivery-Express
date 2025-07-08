2f9ccbf107b99afc9fd20f7285818d717b3a66df
Esteban Samaniego <107878186+MaxiSama12@users.noreply.github.com>
2025-07-08 14:48:08 -0300

    cambiamos la url del backend deployado

--- FRONT/src/router/axiosInstance.js
+++ FRONT/src/router/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
 
-    baseURL: `https://flash-delivery-express-back.onrender.com`,
+    baseURL: `https://flash-delivery-express-back-cs40.onrender.com`,
 
     headers: {
     "ngrok-skip-browser-warning": "true", 
